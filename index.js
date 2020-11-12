00462ba5cae3f560bd89c97c4fa69eaaff3b379f
Isaac Kirk <isaac.kirk9@gmail.com>
2020-11-12 16:48:32 -0600

    "initial commit"

---
+++ index.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+ 
+
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green('This is a short story about bob.'));
+console.log(chalk.magenta('He went to the store and bought some milk'));
+console.log(chalk.red('He drank the milk'));
+chalkAnimation.karaoke('The end');
